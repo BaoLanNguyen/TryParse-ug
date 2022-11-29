d6c5893e3c143844381979a131ee1c0a9bd18048
BaoLanNguyen <bang03001@utb.vaxjo.se>
2022-11-29 11:11:20 +0100

    Initial commit

---
+++ tryparse.cs
@@ -0,0 +1,22 @@
+﻿internal class tryparse
+{
+    public static void Main(string[] args)
+    {
+        int resultat;
+        if (!int.TryParse(Console.ReadLine(), out resultat))
+            Console.WriteLine("Skriv in en siffra");
+    }
+    public static bool TryParse(string s, out int result)
+        {
+            try
+            {
+                result = Convert.ToInt32(s);
+                return true;
+            }
+            catch (System.Exception)
+            {
+                
+                throw;
+            }
+        }
+}
